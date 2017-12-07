cbed5b5acff7a3229c2a35f23dafcbdbabb19a76
Vincent Bett <vincentbett@gmail.com>
2017-12-06 23:01:22 -0500

    Update InsertNodeAtLoc.cpp

--- InsertNodeAtLoc.cpp
+++ InsertNodeAtLoc.cpp
@@ -1,1 +1,43 @@
+/*
+  Insert Node at a given position in a linked list 
+  head can be NULL 
+  First element in the linked list is at position 0
+  Node is defined as 
+  struct Node
+  {
+     int data;
+     struct Node *next;
+  }
+*/
+Node* InsertNth(Node *head, int data, int position)
+{
+  // Complete this method only
+  // Do not write main function. 
+    Node *toAdd = new Node;
+    toAdd->data = data;
+    
+    if(position == 0)
+    {
+        toAdd->next = head;
+        head = toAdd;
+    }
+    else
+    {
+        Node *temp = head;
+        for(int i = 0; i < position - 1; i++)
+        {
+            temp = temp->next;
+        }
+        if(temp->next != NULL)
+        {
+           toAdd->next = temp->next; 
+        }
+        else
+        {
+            toAdd->next = NULL;
+        }
+        temp->next = toAdd;
+    }
+    return head;
+}
 
